41121f89478b181f1c9aad5c1044e1ce3f15f0da
Raul Enrique Torres Reyes <raul20xx@hotmail.com>
2019-12-07 22:05:05 -0600

    Fin del capitulo 4

---
+++ orm_sqlfan/libreria/models/libros_cronica.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from .libros import Libro
+
+
+class LibroCronica(models.Model):
+    descripcion_larga = models.TextField(null=True)
+    libro = models.OneToOneField(Libro, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return "%s Cronica del libro " % self.libro.isb
