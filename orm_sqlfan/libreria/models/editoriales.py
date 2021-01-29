702a6e7abf2e318d1f467d289a42ce3c1cd98bfc
RAUL ENRIQUE TORRES REYES <RAUL.TORRES@cusaem.gob.mx>
2021-01-29 13:34:24 -0600

    Prueba de serializadores

--- orm_sqlfan/libreria/models/editoriales.py
+++ orm_sqlfan/libreria/models/editoriales.py
@@ -10,7 +10,10 @@ from django.db import models
 
 class Editorial(models.Model):
     nombre = models.CharField(max_length=100)
-
+    pais = models.CharField(max_length = 100, null=True)
     class Meta:
         managed = True
         db_table = 'libreria_editorial'
+
+    # def __str__(self):
+    #     return f'Yo soy la editorial {self.nombre}'
--- orm_sqlfan/libreria/views.py
+++ orm_sqlfan/libreria/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import viewsets
 
+from libreria.models.editoriales import Editorial
+from libreria.models.libros import Libro
+
+from libreria.serializers import EditorialSerializerBasico,LibroSerializer_Hyper
 # Create your views here.
+
+class EditorialViewSet(viewsets.ModelViewSet):
+    queryset = Editorial.objects.all()
+    serializer_class = EditorialSerializerBasico
+
+
+class LibroViewSet(viewsets.ModelViewSet):
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer_Hyper
