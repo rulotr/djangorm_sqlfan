10573cb680fa51f2fc375f2b45ebedc5072753d2
raultr <RAUL.TORRES@cusaem.gob.mx>
2021-03-30 11:39:45 -0600

    Se agrego la referencia de django-rest

--- orm_sqlfan/libreria/views.py
+++ orm_sqlfan/libreria/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+# Django Rest Framework
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
 
 @api_view(['GET'])
