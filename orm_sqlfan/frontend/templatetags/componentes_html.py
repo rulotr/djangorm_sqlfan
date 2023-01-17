e901c7c59fa21938f5faabfd70b21901f1fc792b
raultr <RAUL.TORRES@cusaem.gob.mx>
2023-01-16 20:26:56 -0600

    reutilizando  mas templates de django

--- orm_sqlfan/frontend/templatetags/componentes_html.py
+++ orm_sqlfan/frontend/templatetags/componentes_html.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.inclusion_tag("componentes/boton_busqueda.html")
 def mostrar_boton(filtro):
     return {"filtro": filtro}
+
+@register.inclusion_tag("componentes/caja_texto.html")
+def mostrar_caja_texto(titulo, campo, tipo):
+    return {"titulo": titulo,"campo": campo, "tipo": tipo }
